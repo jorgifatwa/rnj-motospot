bdd8a33081aec2c50a7c2f41446d10966b5f61a6
jorgifatwa <jorgifatwaambia@gmail.com>
2024-05-31 04:10:04 +0700

    first push

---
+++ application/views/admin/layouts/sidemenu.php
@@ -0,0 +1,16 @@
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+	<!-- Brand Logo -->
+	<a href="<?php echo base_url() ?>" class="brand-link">
+		<img src="<?php echo base_url() ?>assets/images/icon.png" alt="Combucha Logo" class="brand-image" style="opacity: .8; margin-left: 5px;">
+		<span class="brand-text font-weight-light">Combucha</span>
+	</a>
+
+	<!-- Sidebar -->
+	<div class="sidebar">
+		<nav class="mt-2">
+			<?php echo $menu ?>
+		</nav>
+		<!-- /.sidebar-menu -->
+	</div>
+	<!-- /.sidebar -->
+</aside>

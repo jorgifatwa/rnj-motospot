63ea0b6611a50f5e7fa046361eeb89906044b72a
jorgifatwa <jorgifatwaambia@gmail.com>
2024-06-01 13:45:33 +0700

    fix side menu

--- application/views/admin/layouts/sidemenu.php
+++ application/views/admin/layouts/sidemenu.php
@@ -1,8 +1,8 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
 	<!-- Brand Logo -->
 	<a href="<?php echo base_url() ?>" class="brand-link">
-		<img src="<?php echo base_url() ?>assets/images/icon.png" alt="Combucha Logo" class="brand-image" style="opacity: .8; margin-left: 5px;">
-		<span class="brand-text font-weight-light">Combucha</span>
+		<!-- <img src="<?php echo base_url() ?>assets/images/logo-icon-rnj.png" alt="Combucha Logo" class="brand-image" style="opacity: .8; margin-left: 5px;"> -->
+		<span class="brand-text font-weight-light">RNJ Motospot</span>
 	</a>
 
 	<!-- Sidebar -->
